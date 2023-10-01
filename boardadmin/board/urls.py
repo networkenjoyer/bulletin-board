604ec542cea5bda41d8cea71cef4f7d3edac5635
Alexander Zayats <bidjo0520@gmail.com>
2023-10-01 18:34:36 +0300

    Fix adding photo in adv card

--- boardadmin/board/urls.py
+++ boardadmin/board/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import *
 
@@ -6,3 +8,7 @@ urlpatterns = [
     path('', index, name = 'search_adv'),
     path('addadv/', addadv, name = 'add_adv'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
